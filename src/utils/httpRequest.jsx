a5fe077f6fd09d7cc984ed7e0c431a287ebd1e7b
Hancer <hancermercedes@gmail.com>
2023-10-01 18:41:31 -0400

    adding the env file and configuring the env variable with vite reservation word to get my token variable

--- src/utils/httpRequest.jsx
+++ src/utils/httpRequest.jsx
@@ -1,11 +1,12 @@
 const endpoint = "https://api.themoviedb.org/3";
 
 export const GetRequest = (path) => {
+  console.log(import.meta.env.VITE_TMDB_TOKEN);
   return fetch(`${endpoint}${path}`, {
     headers: {
       Authorization:
         // eslint-disable-next-line no-undef
-        `Bearer ${process.env.TOKEN}`,
+        `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
       "content-type": "application/json; charset=utf-8",
     },
   }).then((result) => result.json());
