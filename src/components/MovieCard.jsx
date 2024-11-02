240de354141b442fa1dd15d8ea80783237622fae
Hancer <hancermercedes@gmail.com>
2024-11-01 21:25:24 -0400

    passing the width and the height to the css file

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,8 +9,6 @@ export const MovieCard = ({ movie }) => {
     <li className={styles.MovieCard}>
       <Link to={`/movies/${movie.id}`}>
         <img
-          width={230}
-          height={345}
           src={imageUrl}
           alt={movie.title}
           className={styles.MovieCardImage}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Search } from "../components/Search";
 import { useDebounce } from "../hooks/useDebounce";
 import { useSearchParams } from "react-router-dom";
 
-/*Note: here we pass the search as a key to reutilized the components that we already has and pass the search by props to used in the child component */
+/*Note: here we pass the search as a key to re-utilize the components that we already has and pass the search by props to used in the child component */
 export const LandingPage = () => {
   const [query] = useSearchParams();
   const search = query.get("search");
