ce9814f62c8b96b0bec510c594ec0e482c03ef81
Hancer <hancermercedes@gmail.com>
2024-12-08 15:51:32 -0400

    updating to React v19

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,6 @@ import { MovieList } from "../components/MovieList";
 import { Search } from "../components/Search";
 import { useDebounce } from "../hooks/useDebounce";
 import { useSearchParams } from "react-router-dom";
-
 /*Note: here we pass the search as a key to re-utilize the components that we already has and pass the search by props to used in the child component */
 export const LandingPage = () => {
   const [query] = useSearchParams();
